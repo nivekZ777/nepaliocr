ee015b165df903f6a32ddebfc1a7077f13f70988
pandey.pandey <pandey.pandey@8a8ed1e1-a44f-0410-ae14-97a372318918>
2009-11-06 01:28:25 +0000

    OCR Menus readded.

--- trunk/Line.h
+++ trunk/Line.h
@@ -1,4 +1,22 @@
 #pragma once
+/**
+ *
+ * Date 07-01-2008
+ *
+ * Author Rajesh Pandey
+ *
+ * license
+ * This code is absolutely free to use and modify. The code is provided "as is" with
+ * no expressed or implied warranty. The author accepts no liability if it causes
+ * any damage to your computer, causes your pet to fall ill, increases baldness
+ * or makes your car start emitting strange noises when you start it up.
+ * This code has no bugs, just undocumented features!
+ * 
+ *  
+ *
+ *  
+ *
+ */
 #include "Word.h"
 
 class Line
