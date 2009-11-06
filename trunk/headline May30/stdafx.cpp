3fca548fa9ba1d59b26c45c56376c0a750b50f76
pandey.pandey <pandey.pandey@8a8ed1e1-a44f-0410-ae14-97a372318918>
2009-11-06 00:18:39 +0000

    may 30

---
+++ trunk/headline May30/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Headliner_ver_1.03.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
